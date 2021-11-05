e92d1b543c3d010c0558580fe5a55763ef0feb70
b-r-hamilton <bydd101@gmail.com>
2021-11-05 17:30:39 -0400

    reorganized, better plots

--- scripts/tsa.jl
+++ scripts/tsa.jl
@@ -1,63 +1,88 @@
 using tele
-using NCDatasets, Revise, Statistics, PyPlot, CFTime, DSP, NaNMath
+using NCDatasets, Revise, Statistics, PyPlot
+using CFTime, DSP, NaNMath, FFTW
+using DataFrames
 #analyzing data from https://psl.noaa.gov/data/gridded/data.noaa.ersst.v5.html
 
+#FILE PATHS
 data_dir = "/home/brynn/Documents/JP/860/tsa_data/"
 ersst_p = data_dir * "sst.mnmean.nc"
-nc = NCDataset(ersst_p)
+cesm_p2 = data_dir * "tos_Omon_CCSM4_past1000_r1i1p1_135001-185012.nc"
 
+#READ NETCDFS TO BOUNDING BOX
 bbox = [-72, -60, 300, 315]
 (late, lone, timee, datae) = read_netcdf(ersst_p, "sst", bbox)
-anome = anomaly(datae, timee)
-
-tavge = mean(anome, dims = (1,2))[1,1,:]
-
-#cesm_p1 = data_dir * "tos_Omon_CCSM4_past1000_r1i1p1_085001-134912.nc"
-cesm_p2 = data_dir * "tos_Omon_CCSM4_past1000_r1i1p1_135001-185012.nc"
 (latc, lonc, timec, datac) = read_netcdf(cesm_p2, "tos", bbox)
-timec = reinterpret(typeof(timee[begin]), timec)
-replace!(datac, missing=>NaN)
-datac = Float32.(datac)
-anomc = anomaly(datac, timec)
 
-# contplot(latc,lonc,mean(anomc, dims = 3)[:,:])
-# contplot(late,lone,mean(anome, dims = 3)[:,:])
-
-
-tavgc = mean(anomc, dims = (1,2))[1,1,:]
-
-figure()
-subplot(1,2,2)
-plot(timee, tavge)
-title("ERSST")
-xlabel("time")
-
-responsetype = Lowpass(0.01*2, fs = 1)
+#FILTER CHARACTERISTICS
+responsetype = Lowpass(0.1*10^(-7),fs = 3.80265176 * 10^(-7))
 designmethod = Butterworth(4)
-tavge_filt = filt(digitalfilter(responsetype, designmethod), tavge)
-plot(timee, tavge_filt)
-legend(["ERSST", "ERSST smoothed"])
 
-subplot(1,2,1)
-plot(timec, tavgc)
-tavgc_filt = filt(digitalfilter(responsetype, designmethod), tavgc)
-plot(timec, tavgc_filt)
-title("CESM")
-legend(["CESM", "CESM smoothed"])
-xlabel("time")
-ylabel("SST Anomaly [°C]")
+#COMPUTE TIME SERIES (MEAN OVER BBOX, MEAN ANOMALY OVER BBOX, FILTER EACH)
+#ersst
+anome = anomaly(datae, timee)
+tavge_anom = mean(anome, dims = (1,2))[1,1,:]
+tavge_raw = mean(datae, dims = (1,2))[1,1,:]
+tavge_anom_filt = filt(digitalfilter(responsetype, designmethod), tavge_anom)
+tavge_raw_filt = filt(digitalfilter(responsetype, designmethod), tavge_raw)
 
-suptitle("Average Anomaly in ROI, Raw and Smoothed (Butterworth w/ 0.02 month^-1 Lowpass Filter)")
+#cesm
+timec = reinterpret(typeof(timee[begin]), timec) #convert from DatetimeNoLeap to DateTime
+replace!(datac, missing=>NaN) #convert missing vals to NaN
+datac = Float32.(datac) #convert to Float32 (used to be Union{Missing, Float32} but our missing are gone anyways)
+datac = datac .- 273.15 #convert to celsius
+anomc = anomaly(datac, timec)
+tavgc_anom = mean(anomc, dims = (1,2))[1,1,:]
+replace!(datac, NaN=>0) #convert missing vals to NaN
+tavgc_raw = mean(datac, dims = (1,2))[1,1,:]
+tavgc_anom_filt = filt(digitalfilter(responsetype, designmethod), tavgc_anom)
+tavgc_raw_filt = filt(digitalfilter(responsetype, designmethod), tavgc_raw)
 
-using FFTW
-ffte = fft(tavge)
-ffte_freq = fftfreq(length(timee), 3.80265176 * 10^(-7))
-period = (1 ./ ffte_freq)/3600/24/365
+#Make contour plots
+savetype = ".png"
+contplot(latc,lonc,mean(datac, dims = 3)[:,:], "images/datac"*savetype)
+contplot(late,lone,mean(datae, dims = 3)[:,:], "images/datae"*savetype)
+contplot(latc,lonc,mean(anomc, dims = 3)[:,:], "images/anomc"*savetype)
+contplot(late,lone,mean(anome, dims = 3)[:,:], "images/anome"*savetype)
+
+#Make time series plots
+filtseriesplot(tavgc_raw[6012 - 2013:end], tavgc_raw_filt[6012 - 2013:end], timec[6012 - 2013:end], tavge_raw, tavge_raw_filt, timee, "Average Time Series in ROI", "images/raw"*savetype)
+filtseriesplot(tavgc_anom[6012 - 2013:end], tavgc_anom_filt[6012 - 2013:end], timec[6012 - 2013:end], tavge_anom, tavge_anom_filt, timee, "Average Anomaly Time Series in ROI", "images/anom"*savetype)
+
+#COMPUTE FFTs
+ffte_raw = fft(tavge_raw)
+ffte_anom = fft(tavge_anom)
+ffte_freq = FFTW.fftfreq(length(timee), 3.80265176 * 10^(-7))
+
+fftc_raw = fft(tavgc_raw)
+fftc_anom = fft(tavgc_anom)
+fftc_freq = FFTW.fftfreq(length(timec), 3.80265176 * 10^(-7))
+
+#PLot FFT
+fftplot(ffte_freq[:], [ffte_raw, ffte_anom], "images/ffte"*savetype, "ERSST")
+fftplot(fftc_freq[:], [fftc_raw, fftc_anom], "images/fftc"*savetype, "CESM")
+
+#Plot
 figure()
-scatter(ffte_freq[begin+1:end-1], abs.(ffte)[begin+1:end-1])
-xlim([0, maximum(ffte_freq)])
+halfway = 1006
+plot(ffte_freq[begin+1:halfway], abs.(ffte_raw)[begin+1:halfway], alpha = 0.5)
+halfway = 3006
+plot(fftc_freq[begin+1:halfway], abs.(fftc_raw)[begin+1:halfway], alpha = 0.5)
+ylabel("Frequency [Hz]")
+xlabel("Intensity")
+legend(["ERSST", "CESM"])
+savefig("images/fftec"*savetype)
+close()
 
-function character(series)
-    max = NaNMath.maximum(series)
-    return max
-end
+
+#histogram
+#fig2b - boxplotGPCC Total # of Precips Obs
+figure()
+subplot(1,2,1)
+title("CESM")
+boxplot([tavgc_raw, tavgc_anom], notch = true, labels =  ["Raw", "Anomaly"])
+subplot(1,2,2)
+title("ERSST")
+boxplot([tavge_raw, tavge_anom], notch = true, labels = ["Raw", "Anomaly"])
+savefig("images/boxes"*savetype)
+close()
--- src/tele.jl
+++ src/tele.jl
@@ -1,11 +1,12 @@
 module tele
-using NCDatasets, Dates, PyCall, Revise, PyPlot, Statistics
+using NCDatasets, Dates, PyCall, Revise, PyPlot, Statistics, NaNMath
 
-export read_netcdf, contplot, anomaly, batch_open
+export read_netcdf, contplot, anomaly, batch_open, character, filtseriesplot, fftplot
 
 const ccrs = PyNULL()
 const cfeature = PyNULL()
 const mpatches = PyNULL()
+const ticker = PyNULL()
 
 """
    function nc_dims(nc)
@@ -88,9 +89,10 @@ function load_python()
    copy!(ccrs, pyimport("cartopy.crs"))
    copy!(cfeature, pyimport("cartopy.feature"))
    copy!(mpatches, pyimport("matplotlib.patches"))
+   copy!(ticker, pyimport("matplotlib.ticker"))
 end
 
-function contplot(lat,lon, data)
+function contplot(lat,lon, data,s)
    load_python()
    figure()
    ax = subplot(projection = ccrs.SouthPolarStereo())
@@ -99,6 +101,8 @@ function contplot(lat,lon, data)
    ax.coastlines()
    cf = contourf(lon,lat,transpose(data), cmap = "coolwarm", transform=ccrs.PlateCarree())
    colorbar(cf)
+   savefig(s)
+   close()
 end
 
 function anomaly(data, time)
@@ -130,6 +134,87 @@ function anomaly(data, time)
    replace!(anomaly, NaN=>0)
    return anomaly
 
+end
+
+function character(series)
+    min = NaNMath.minimum(series)
+    max = NaNMath.maximum(series)
+    mean = NaNMath.mean(series)
+    median = NaNMath.median(series)
+    std = NaNMath.std(series)
+    dict = Dict("min"=>min, "max"=> max, "mean"=>mean, "median"=>median, "std"=>std)
+    return dict
+end
+
+function filtseriesplot(series1, series1filt, series1time, series2, series2filt, series2time, t, s)
+    figure(figsize = (10, 4))
+    subplot(1,2,1)
+    plot(series1time, series1)
+    plot(series1time[2:end], series1filt[2:end])
+    x_ind1 = range(1, stop = length(series1time), step = 1)
+    series1filt_slope = normalequation(x_ind1, series1filt)
+    series1filt_ne = series1filt_slope*x_ind1
+    plot(series1time, series1filt_ne, color = "r")
+    text(x = Date(1700,1,1), y = 3, s = "slope = " *string(round(series1filt_slope, digits =6)))
+    legend(["CESM", "CESM smoothed", "CESM smoothed trend"])
+    xlabel("time")
+    ylabel("SST Anomaly [°C]")
+
+    subplot(1,2,2)
+    plot(series2time, series2)
+    plot(series2time[2:end], series2filt[2:end])
+    x_ind2 = range(1, stop = length(series2time[20:end]), step = 1)
+    series2filt_slope = normalequation(x_ind2, series2filt[20:end])
+    series2filt_ne = series2filt_slope*x_ind2
+    plot(series2time[20:end], series2filt_ne, color = "r")
+    text(x = Date(1880,1,1), y = 3, s = "slope = " *string(round(series2filt_slope, digits =6)))
+    xlabel("time")
+    legend(["ERSST", "ERSST smoothed", "ERSST smoothed trend"])
+    suptitle(t)
+    savefig(s)
+    close()
+end
+
+function fftplot(ffte_freq, ffte, s, t)
+   load_python()
+   figure()
+   ax = subplot()
+   N = length(ffte_freq)
+   halfway = 0.0
+   if N % 2 == 1
+      halfway = convert(Int, N/2 - 0.5 )
+   else
+      halfway = convert(Int, N/2)
+   end
+   replace!(ffte_freq, 0.0 => 1.0)
+   print(halfway)
+
+   if typeof(ffte[1]) in [Vector{ComplexF32}, Vector{ComplexF64}]
+      for i in ffte
+         ax.plot(ffte_freq[begin+1:halfway], abs.(i)[begin+1:halfway], linewidth = 2, alpha = 0.5)
+      end
+      legend(["Raw", "Anomaly"])
+   else
+      ax.plot(ffte_freq[begin+1:halfway], abs.(ffte)[begin+1:halfway])
+   end
+   xt = range(ffte_freq[begin + 1], stop = ffte_freq[halfway], step = 2*10^(-10) * 100)
+   ax.set_xticks(xt)
+   ax.set_xlabel("Frequency [Hz]")
+   ax.set_ylabel("Intensity")
+
+   #ax.set_ylim([0, 750])
+   ax.set_xlim(ffte_freq[begin+1], ffte_freq[halfway])
+   ax.grid()
+   title(t)
+   savefig(s)
+   close()
+
+end
+
+#linear regression with least square cost function
+function normalequation(x,y)
+   val = inv(transpose(x)*x)*transpose(x)*y
+   return val
 end
 
 end
